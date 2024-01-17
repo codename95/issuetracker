d6e06749c888919b6d35a922aed10174552c0f89
Salem Kosemani <salemsky95@gmail.com>
2024-01-16 21:38:17 -0500

    Update Issues on Issue Form

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -16,8 +16,7 @@ import { Issue } from "@prisma/client";
 
 type IssueFormData = z.infer<typeof issueSchema>;
 
- 
-const IssueForm = ({issue}: {issue?: Issue}) => {
+const IssueForm = ({ issue }: { issue?: Issue }) => {
   const router = useRouter();
   const {
     register,
@@ -32,7 +31,8 @@ const IssueForm = ({issue}: {issue?: Issue}) => {
   const onSubmit = handleSubmit(async (data) => {
     try {
       setisSubmitting(true);
-      await axios.post("/api/issues", data);
+      if (issue) await axios.patch("/api/issues/" + issue.id, data);
+      else await axios.post("/api/issues", data);
       router.push("/issues");
     } catch (error) {
       setisSubmitting(false);
@@ -48,7 +48,8 @@ const IssueForm = ({issue}: {issue?: Issue}) => {
       )}
       <form className="space-y-3" onSubmit={onSubmit}>
         <TextField.Root>
-          <TextField.Input defaultValue={issue?.title}
+          <TextField.Input
+            defaultValue={issue?.title}
             placeholder="Enter Title of Issue…"
             {...register("title")}
           />
@@ -64,7 +65,8 @@ const IssueForm = ({issue}: {issue?: Issue}) => {
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Button disabled={isSubmitting}>
-          Submit New Issue {isSubmitting && <Spinner />}
+          {issue ? "Update Issue" : "Submit New Issue"}
+          {isSubmitting && <Spinner />}
         </Button>
       </form>{" "}
     </div>
